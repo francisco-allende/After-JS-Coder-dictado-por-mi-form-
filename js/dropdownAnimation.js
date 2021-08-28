9c0abcbf15fc68758bb3635ddd463473f768e9b2
francisco-allende <74782019+francisco-allende@users.noreply.github.com>
2021-08-27 21:27:51 -0300

    Add files via upload

---
+++ js/dropdownAnimation.js
@@ -0,0 +1,43 @@
+//Form dropdwdown and card animation
+let cardDrop = $('#card-dropdown');
+let activeDropdown;
+cardDrop.click(function(){
+  let node;
+  for (var i = 0; i < this.childNodes.length-1; i++)
+    node = this.childNodes[i];
+    if (node.className === 'dropdown-select') {
+      node.classList.add('visible');
+       activeDropdown = node; 
+    };
+})
+
+let x = document.querySelector('.ventana_formulario_completa')
+x.onclick = function(e) {
+  if (e.target.tagName === 'LI' && activeDropdown){
+    if (e.target.innerHTML === 'Master Card') {
+      document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/2vbqk5lcpi7hjoc/MasterCard_Logo.svg.png';
+          activeDropdown.classList.remove('visible');
+      activeDropdown = null;
+      e.target.innerHTML = document.getElementById('current-card').innerHTML;
+      document.getElementById('current-card').innerHTML = 'Master Card';
+    }
+    else if (e.target.innerHTML === 'American Express') {
+         document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/f5hyn6u05ktql8d/amex-icon-6902.png';
+          activeDropdown.classList.remove('visible');
+      activeDropdown = null;
+      e.target.innerHTML = document.getElementById('current-card').innerHTML;
+      document.getElementById('current-card').innerHTML = 'American Express';      
+    }
+    else if (e.target.innerHTML === 'Visa') {
+         document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/ubamyu6mzov5c80/visa_logo%20%281%29.png';
+          activeDropdown.classList.remove('visible');
+      activeDropdown = null;
+      e.target.innerHTML = document.getElementById('current-card').innerHTML;
+      document.getElementById('current-card').innerHTML = 'Visa';
+    }
+  }
+  else if (e.target.className !== 'dropdown-btn' && activeDropdown) {
+    activeDropdown.classList.remove('visible');
+    activeDropdown = null;
+  }
+}
---
+++ js/form.js
@@ -0,0 +1,137 @@
+(function () {
+    //Creo el form con un slideDown
+    let counter = 0;
+    const createForm = () => {
+        $('#btn_crear_form').click(()=>{
+            $('.ventana_formulario_completa').slideToggle(1000);
+    
+            counter++;
+            if(counter%2 !=0){
+                $('#btn_crear_form').html('Ocultar');
+                $('#borrar_datos_form').show();
+            }else{
+                $('#btn_crear_form').html('Crea tu formulario!');
+                $('#borrar_datos_form').hide();
+            }
+        })
+    }
+    createForm();
+
+    //Guardo los inputs de valores en variables
+    let nombre = $('#nombre');
+    let telefono = $("#telefono");
+    let direccion = $("#direccion");
+    let nroTarjeta = $('#nro_tarjeta');
+    let nombreTitular = $('#nombre_titular');
+    let expiracion = $('#expiracion');
+    let codigoSeguridad = $('#codigo_seguridad');
+    //Los guardo en un objeto y los guardo vacios
+    let datosPersona = {
+        name: "", 
+        phone: "", 
+        adress: "", 
+        cardNumber: "", 
+        ownerName: "", 
+        expiration:"", 
+        securityCode:""
+    }
+    
+    //Guardo en un localStorage los valores del objeto datosPersona
+    //.val() para sustraer los datos del form
+    //datosPersona.algo --> ese algo es el value de cada property que vemos en el storage
+    const guardoDatosCargados = () => {
+        datosPersona.name = nombre.val(); 
+        datosPersona.phone = telefono.val();
+        datosPersona.adress = direccion.val(); 
+        datosPersona.cardNumber = nroTarjeta.val();
+        datosPersona.ownerName = nombreTitular.val();
+        datosPersona.expiration = expiracion.val();
+        datosPersona.securityCode = codigoSeguridad.val();
+        //Valido antes de enviar y guardar
+        if(datosPersona.name != "" && datosPersona.phone !="" && datosPersona.adress !="" && datosPersona.cardNumber !="" && datosPersona.ownerName !="" && datosPersona.expiration !="" && datosPersona.securityCode !=""){
+            //Guardo en el localStorage este objeto cargado con valores. Me ahorro de hacer un set item para cada uno
+            //El método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON
+            localStorage.datosPersona = JSON.stringify(datosPersona);  
+            domCompra(datosPersona.name, datosPersona.adress);
+        }else{
+            alert('Rellena todos los datos 😅')  
+        } 
+    }
+
+    $('#btnConfirmarCompra').click(()=>{     
+        guardoDatosCargados();
+    })  
+    
+    $('#borrar_datos_form').click(()=>{
+        localStorage.removeItem('datosPersona'); 
+    }) 
+
+    const domCompra = (name, adress) => {
+        $('.ventana_formulario_completa').fadeOut(800);
+        $('#btn_crear_form').hide();
+        $('#borrar_datos_form').hide();
+        $('#compra_exitosa').append(
+                `
+                <h1>Gracias por tu compra ${name}</h1>
+                <h2>🛵 Un delivery esta yendo a ${adress} con tu pedido!🛵</h2>
+                `
+            )
+    }
+    
+    
+
+  //Method.change --> guarda en el local storage los datos antes de darle a confirmar
+  nombre.change(guardoDatosCargados),
+  telefono.change(guardoDatosCargados),
+  direccion.change(guardoDatosCargados),
+  nroTarjeta.change(guardoDatosCargados),
+  nombreTitular.change(guardoDatosCargados),
+  expiracion.change(guardoDatosCargados),
+  codigoSeguridad.change(guardoDatosCargados)
+
+
+  
+  //Method.focus --> cambia el estilo con un click al input
+  nombre.focus(function(){
+    nombre.css('background', 'lightblue')
+  }),
+  telefono.focus(function(){
+    telefono.css('background', 'lightblue')
+  }),
+  direccion.focus(function(){
+    direccion.css('background', 'lightblue')
+  });
+  nroTarjeta.focus(function(){
+    nroTarjeta.css('background', '#D3D3D3')
+  }),
+  nombreTitular.focus(function(){
+    nombreTitular.css('background', '#D3D3D3')
+  }),
+  expiracion.focus(function(){
+    expiracion.css('background', '#D3D3D3')
+  }),
+  codigoSeguridad.focus(function(){
+    codigoSeguridad.css('background', '#D3D3D3')
+  });
+  
+  
+  
+  //Method.blur() --> cambia el color ya una vez clickeado
+  nroTarjeta.blur(function(){
+    nroTarjeta.css('background', '#8A2BE2')
+  }),
+  nombreTitular.blur(function(){
+    nombreTitular.css('background', '#8A2BE2')
+  }),
+  expiracion.blur(function(){
+    expiracion.css('background', '#8A2BE2')
+  }),
+  codigoSeguridad.blur(function(){
+    codigoSeguridad.css('background', '#8A2BE2')
+  });
+  
+})();
+
+
+
+   
